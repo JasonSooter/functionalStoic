b59222c80b4c6fb99787da12691dd41b6d2c5ae7
functionalStoic <7215306+functionalStoic@users.noreply.github.com>
2025-12-29 20:42:09 -0700

    Quartz sync: Dec 29, 2025, 8:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: { provider: "plausible" },
     locale: "en-US",
     baseUrl: "notes.jasonsooter.com",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "people/family/**"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
